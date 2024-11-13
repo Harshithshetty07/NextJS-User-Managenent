e6aed2551a92f6cc75eeaaf9277782aaacc999e3
Harshith KV <harshith.shetty2002@gmail.com>
2024-11-13 15:20:02 +0530

    add

---
+++ frontend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    process.env.NEXT_PUBLIC_SITE_URL,
+    'https://your-vercel-domain.vercel.app',
+    'http://localhost:3000'
+  ];
+  
+  export default allowedOrigins;
